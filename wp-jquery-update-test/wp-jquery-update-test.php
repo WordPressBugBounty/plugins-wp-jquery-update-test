db45511e9b5ef7d2e570351354f6e89fbf862e64
Sean Murphy <sean@iamseanmurphy.com>
2025-01-22 00:34:05 +0000

    3.0.2

--- wp-jquery-update-test/wp-jquery-update-test.php
+++ wp-jquery-update-test/wp-jquery-update-test.php
@@ -3,9 +3,9 @@
  * Plugin Name: Test jQuery Updates
  * Plugin URI: https://wordpress.org/plugins/wp-jquery-update-test
  * Description: A feature plugin to help with testing updates of the jQuery and jQuery UI JavaScript libraries (not intended for use in production).
- * Version: 3.0.1
+ * Version: 3.0.2
  * Requires at least: 6.5
- * Tested up to: 6.7
+ * Tested up to: 6.8
  * Requires PHP: 7.0
  * Author: The WordPress Team
  * Author URI: https://wordpress.org
@@ -21,6 +21,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Only for WP 6.5, 6.6, and 6.7 (alpha, beta, RC, and release)
-if ( version_compare( $GLOBALS['wp_version'], '6.5', '>=' ) && version_compare( $GLOBALS['wp_version'], '6.8-alpha', '<' ) ) {
+if ( version_compare( $GLOBALS['wp_version'], '6.5', '>=' ) && version_compare( $GLOBALS['wp_version'], '6.9-alpha', '<' ) ) {
 	include_once __DIR__ . '/class_wp_jquery_update_test.php';
 }
